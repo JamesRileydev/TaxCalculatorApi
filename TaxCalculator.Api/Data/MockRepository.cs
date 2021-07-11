9963f5f690c9572beff5401ad44f65024277bc1f
JamesRileydev <jamesriley.dev@gmail.com>
2021-07-11 10:40:06 -0400

    Refactored MockRepository to have only one method to retrieve tax rates

--- TaxCalculator.Api/Data/MockRepository.cs
+++ TaxCalculator.Api/Data/MockRepository.cs
@@ -10,38 +10,36 @@ namespace TaxCalculator.Api.Data
     //
     // Values that can change such as tax rates would be stored
     // in a database or some other common location that would 
-    // be maintained by some other entity. What the actual tax 
-    // rate is is not the concern of the service using it.
+    // be maintained by some other entity, such as accounting.
+    // What the actual tax rate is is not the concern of
+    // the service using it.
     //
     // For the insert I'm assuming we are using a relational database
     // to keep track of orders
     //---------------------------------------------------------//
     public interface IMockRepository
     {
-        Task<int> GetBasicSalesTaxRateAsync();
-
-        Task<int> GetImportTaxRateAsync();
+        Task<(int, int)> GetTaxRatesAsync();
 
         Task<long> InsertOrderAsync(Order order);
     }
     public class MockRepository : IMockRepository
     {
-        public async Task<int> GetBasicSalesTaxRateAsync()
+        public async Task<(int, int)> GetTaxRatesAsync()
         {
-            await Task.CompletedTask;
-            return 10;
-        }
+            var basicTaxRate = 10;
+            var importTaxRate = 5;
 
-        public async Task<int> GetImportTaxRateAsync()
-        {
             await Task.CompletedTask;
-            return 5;
+            return (basicTaxRate, importTaxRate);
         }
 
         public async Task<long> InsertOrderAsync(Order order)
         {
             // For this I'm assuming the insert was successful
-            // If this were an actual insert I would return the table row ID
+            // If this were an actual insert I might return the
+            // table row ID which could correlate to the order number
+            // Or I could return the number of rows affected
 
             await Task.CompletedTask;
             return 1;
