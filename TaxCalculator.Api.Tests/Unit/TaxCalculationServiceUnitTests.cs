0d0a6d109e275279732e7c91c291305d5a179891
JamesRileydev <jamesriley.dev@gmail.com>
2021-07-11 11:07:22 -0400

    Finished TaxCalculationService unit tests

--- TaxCalculator.Api.Tests/Unit/TaxCalculationServiceUnitTests.cs
+++ TaxCalculator.Api.Tests/Unit/TaxCalculationServiceUnitTests.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutofacContrib.NSubstitute;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using TaxCalculator.Api.Data;
+using TaxCalculator.Api.Enums;
 using TaxCalculator.Api.Models;
 using TaxCalculator.Api.Services;
 using Xunit;
@@ -16,15 +19,206 @@ namespace TaxCalculator.Api.Tests.Unit
         public async Task CalculateTaxAsync_ReturnsServiceError_WhenLINQThrows()
         {
             var autoSub = new AutoSubstitute();
-            var taxSvc = autoSub.Resolve<TaxCalculationService>();
+            var sut = autoSub.Resolve<TaxCalculationService>();
 
-            var (result, error) = await taxSvc.CalculateTaxAsync(null, Guid.NewGuid())
+            var (result, error) = await sut.CalculateTaxAsync(new Order())
                 .ConfigureAwait(false);
 
             Assert.Null(result);
             Assert.NotNull(error);
             Assert.IsType<ServiceError>(error);
             Assert.Contains("An error occurred while combining items", error.Message);
+        }
+
+        [Fact]
+        public async Task CalculateTaxAsync_ReturnsServiceError_WhenMockRepoThrowsWhileGettingTaxRates()
+        {
+            var autoSub = new AutoSubstitute();
+            var sut = autoSub.Resolve<TaxCalculationService>();
+
+            var mockRepo = autoSub.Resolve<IMockRepository>();
+            mockRepo.GetTaxRatesAsync().Throws(new Exception());
+
+            var (result, error) = await sut.CalculateTaxAsync(new Order { OrderItems = new List<OrderItem>() })
+                .ConfigureAwait(false);
+
+            Assert.Null(result);
+            Assert.NotNull(error);
+            Assert.IsType<ServiceError>(error);
+            Assert.Contains("An error occurred while retrieving tax rates", error.Message);
+        }
+
+        [Fact]
+        public async Task CalculateTaxAsync_ReturnsServiceError_WhenMockRepoReturnsZero()
+        {
+            var autoSub = new AutoSubstitute();
+            var sut = autoSub.Resolve<TaxCalculationService>();
+
+            var mockRepo = autoSub.Resolve<IMockRepository>();
+            mockRepo.GetTaxRatesAsync().Returns((0, 1));
+
+            var (result, error) = await sut.CalculateTaxAsync(new Order{ OrderItems = new List<OrderItem>() })
+                .ConfigureAwait(false);
+
+            Assert.Null(result);
+            Assert.NotNull(error);
+            Assert.IsType<ServiceError>(error);
+            Assert.Contains("One or more values were not returned while retrieving the tax rates", error.Message);
+        }
+
+        [Fact]
+        public async Task CalculateTaxAsync_ReturnsServiceError_WhenCalculateTaxAsyncReturnsError()
+        {
+            var order = new Order
+            {
+                OrderItems = new List<OrderItem>(new List<OrderItem>{new OrderItem
+                {
+                    Name = "Name",
+                    Category = ItemCategories.Unknown,
+                    Price = 1.00m
+                }}),
+                CombinedItems = new List<OrderItem>(new List<OrderItem>())
+            };
+
+            var autoSub = new AutoSubstitute();
+            var sut = autoSub.Resolve<TaxCalculationService>();
+
+            var mockRepo = autoSub.Resolve<IMockRepository>();
+            mockRepo.GetTaxRatesAsync().Returns((10, 5));
+
+            var (result, error) = await sut.CalculateTaxAsync(order)
+                .ConfigureAwait(false);
+
+            Assert.Null(result);
+            Assert.NotNull(error);
+            Assert.IsType<ServiceError>(error);
+            Assert.Contains("Unable to calculate tax for item category", error.Message);
+        }
+
+        [Fact]
+        public async Task CalculateTaxAsync_ReturnsReceipt_WhenCalculateBasicTaxAsyncSucceeds()
+        {
+            var order = new Order
+            {
+                OrderItems = new List<OrderItem>(new List<OrderItem>{new OrderItem
+                {
+                    Name = "Name",
+                    Category = ItemCategories.Basic,
+                    Price = 10.00m
+                }}),
+                CombinedItems = new List<OrderItem>(new List<OrderItem>())
+            };
+
+            var autoSub = new AutoSubstitute();
+            var sut = autoSub.Resolve<TaxCalculationService>();
+
+            var mockRepo = autoSub.Resolve<IMockRepository>();
+            mockRepo.GetTaxRatesAsync().Returns((10, 5));
+
+            var (result, error) = await sut.CalculateTaxAsync(order)
+                .ConfigureAwait(false);
+
+            Assert.Null(error);
+            Assert.NotNull(result);
+
+            Assert.IsType<Receipt>(result);
+            Assert.Equal(1.00m, result.Tax);
+            Assert.Equal(11.00m, result.Total);
+        }
+
+        [Fact]
+        public async Task CalculateTaxAsync_ReturnsReceipt_WhenCalculateImportTaxAsyncSucceeds()
+        {
+            var order = new Order
+            {
+                OrderItems = new List<OrderItem>(new List<OrderItem>{new OrderItem
+                {
+                    Name = "Name",
+                    Category = ItemCategories.ExemptImport,
+                    Price = 10.00m
+                }}),
+                CombinedItems = new List<OrderItem>(new List<OrderItem>())
+            };
+
+            var autoSub = new AutoSubstitute();
+            var sut = autoSub.Resolve<TaxCalculationService>();
+
+            var mockRepo = autoSub.Resolve<IMockRepository>();
+            mockRepo.GetTaxRatesAsync().Returns((10, 5));
+
+            var (result, error) = await sut.CalculateTaxAsync(order)
+                .ConfigureAwait(false);
+
+            Assert.Null(error);
+            Assert.NotNull(result);
+
+            Assert.IsType<Receipt>(result);
+            Assert.Equal(0.50m, result.Tax);
+            Assert.Equal(10.50m, result.Total);
+        }
+
+        [Fact]
+        public async Task CalculateTaxAsync_ReturnsReceipt_WhenCalculateBasicAndImportTaxAsyncSucceeds()
+        {
+            var order = new Order
+            {
+                OrderItems = new List<OrderItem>(new List<OrderItem>{new OrderItem
+                {
+                    Name = "Name",
+                    Category = ItemCategories.BasicImport,
+                    Price = 10.00m
+                }}),
+                CombinedItems = new List<OrderItem>(new List<OrderItem>())
+            };
+
+            var autoSub = new AutoSubstitute();
+            var sut = autoSub.Resolve<TaxCalculationService>();
+
+            var mockRepo = autoSub.Resolve<IMockRepository>();
+            mockRepo.GetTaxRatesAsync().Returns((10, 5));
+
+            var (result, error) = await sut.CalculateTaxAsync(order)
+                .ConfigureAwait(false);
+
+            Assert.Null(error);
+            Assert.NotNull(result);
+
+            Assert.IsType<Receipt>(result);
+            Assert.Single(result.OrderItems);
+            Assert.Equal(1.50m, result.Tax);
+            Assert.Equal(11.50m, result.Total);
+        }
+
+        [Fact]
+        public async Task CalculateTaxAsync_ReturnsReceipt_WhenItemIsExcempt()
+        {
+            var order = new Order
+            {
+                OrderItems = new List<OrderItem>(new List<OrderItem>{new OrderItem
+                {
+                    Name = "Name",
+                    Category = ItemCategories.Exempt,
+                    Price = 10.00m
+                }}),
+                CombinedItems = new List<OrderItem>(new List<OrderItem>())
+            };
+
+            var autoSub = new AutoSubstitute();
+            var sut = autoSub.Resolve<TaxCalculationService>();
+
+            var mockRepo = autoSub.Resolve<IMockRepository>();
+            mockRepo.GetTaxRatesAsync().Returns((10, 5));
+
+            var (result, error) = await sut.CalculateTaxAsync(order)
+                .ConfigureAwait(false);
+
+            Assert.Null(error);
+            Assert.NotNull(result);
+
+            Assert.IsType<Receipt>(result);
+            Assert.Single(result.OrderItems);
+            Assert.Equal(0m, result.Tax);
+            Assert.Equal(10.00m, result.Total);
         }
     }
 }
